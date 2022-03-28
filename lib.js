649523ac33935d0ccd0229c823ede5b94a66d04a
robertocannella <robertocannella@gmail.com>
2022-03-28 14:25:14 -0400

    added FizzBuzz Exercise

--- lib.js
+++ lib.js
@@ -18,7 +18,7 @@ module.exports.getCurrencies = function () {
 
 // Testing objects 
 module.exports.getProduct = function (productId) {
-  return { id: productId, price: 10 };
+  return { id: productId, price: 10, categore: 'none' };
 }
 
 // Testing exceptions 
@@ -41,4 +41,20 @@ module.exports.notifyCustomer = function (order) {
   const customer = db.getCustomerSync(order.customerId);
 
   mail.send(customer.email, 'Your order was placed successfully.');
+}
+
+module.exports.fizzBuzz = function (input) {
+  if (typeof input !== 'number')
+    throw new Error('Input should be a number');
+
+  if ((input % 3 === 0) && (input % 5) === 0)
+    return 'FizzBuzz';
+
+  if (input % 3 === 0)
+    return 'Fizz';
+
+  if (input % 5 === 0)
+    return 'Buzz';
+
+  return input;
 }
--- tests/lib.test.js
+++ tests/lib.test.js
@@ -43,3 +43,57 @@ describe('getCurrencies', () => {
         expect(result).toEqual(expect.arrayContaining(['EUR', 'AUD', 'USD']))
     })
 });
+
+describe('getProduct', () => {
+    it('should return the product with the given id', () => {
+        const result = lib.getProduct(1)
+        // expect(result).toBe({ id: 1, price: 10 }); <-- toBe references memory location
+        //expect(result).toEqual({ id: 1, price: 10 }); // <-- must much exact properties
+        expect(result).toMatchObject({ id: 1, price: 10 }); // <-- match object with at least these properties
+        expect(result).toHaveProperty('id', 1); // <-- verify specific property exists
+    })
+});
+
+describe('registerUser', () => {
+    it('should throw if username is falsy', () => {
+        const falsyValues = [null, undefined, NaN, '', 0, false];
+        falsyValues.forEach((value) => {
+            expect(() => { lib.registerUser(value) }).toThrow();
+        });
+    });
+
+    it('should return the username and its assigned id as DateTime', () => {
+        const result = lib.registerUser('robertocannella');
+        expect(result).toMatchObject({ username: 'robertocannella' });
+        expect(result.id).toBeGreaterThan(0);
+    });
+});
+
+describe('fizzBuzz', () => {
+    it('should throw if input is NaN', () => {
+        expect(() => { lib.fizzBuzz(undefined) }).toThrow();
+        expect(() => { lib.fizzBuzz("a") }).toThrow();
+        expect(() => { lib.fizzBuzz(null) }).toThrow();
+        expect(() => { lib.fizzBuzz("12") }).toThrow();
+    });
+
+    it('should return "FizzBuzz" if it is divisible by 3 and 5', () => {
+        const result = lib.fizzBuzz(15);
+        expect(result).toBe('FizzBuzz');
+    });
+
+    it('should return "Fizz" if it is divisible by 3', () => {
+        const result = lib.fizzBuzz(9);
+        expect(result).toBe('Fizz');
+    })
+
+    it('should return "Buzz" if it is divisible by 5', () => {
+        const result = lib.fizzBuzz(20);
+        expect(result).toBe('Buzz');
+    })
+
+    it('should return input if it is not divisible by 3 or 5', () => {
+        const result = lib.fizzBuzz(1);
+        expect(result).toBe(1);
+    })
+});
