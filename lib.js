5fd6a988951263f82600ad67c992cacf7ac355fa
robertocannella <robertocannella@gmail.com>
2022-03-27 22:19:45 -0400

    refactored lib.absolute()

--- lib.js
+++ lib.js
@@ -2,44 +2,42 @@ const db = require('./db');
 const mail = require('./mail');
 
 // Testing numbers 
-module.exports.absolute = function(number) {
-  if (number > 0) return number; 
-  if (number < 0) return -number; 
-  return 0; 
+module.exports.absolute = function (number) {
+  return (number >= 0) ? number : -number;
 }
 
 // Testing strings 
-module.exports.greet = function(name) { 
-  return 'Welcome ' + name; 
+module.exports.greet = function (name) {
+  return 'Welcome ' + name;
 }
 
 // Testing arrays 
-module.exports.getCurrencies = function() { 
+module.exports.getCurrencies = function () {
   return ['USD', 'AUD', 'EUR'];
 }
 
 // Testing objects 
-module.exports.getProduct = function(productId) { 
+module.exports.getProduct = function (productId) {
   return { id: productId, price: 10 };
 }
 
 // Testing exceptions 
-module.exports.registerUser = function(username) { 
+module.exports.registerUser = function (username) {
   if (!username) throw new Error('Username is required.');
 
   return { id: new Date().getTime(), username: username }
 }
 
 // Mock functions 
-module.exports.applyDiscount = function(order) { 
+module.exports.applyDiscount = function (order) {
   const customer = db.getCustomerSync(order.customerId);
 
-  if (customer.points > 10) 
-    order.totalPrice *= 0.9; 
+  if (customer.points > 10)
+    order.totalPrice *= 0.9;
 }
 
 // Mock functions 
-module.exports.notifyCustomer = function(order) { 
+module.exports.notifyCustomer = function (order) {
   const customer = db.getCustomerSync(order.customerId);
 
   mail.send(customer.email, 'Your order was placed successfully.');
